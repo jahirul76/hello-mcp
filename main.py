7db758c62a129dc3e4e53a352f605f70afb1c151
Jahirul Islam <jislam@cloudera.com>
2026-01-08 17:17:35 +0000

    hello mcp server

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from os import name
+from mcp.server.fastmcp import FastMCP
+
+# Initialise FastMCP server
+mcp = FastMCP("reliefweb")
+
+@mcp.tool(name="hello", description="A simple hello world tool")
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+
+def main():
+    print("Hello from hello-mcp!")
+
+    # Initialise and run the server
+    mcp.run(transport="stdio")
+
+
+if __name__ == "__main__":
+    main()
